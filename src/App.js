f5d66f911dd86caec041a3606af0a81d019be5f4
Julian Baez <Julianbsuarez@hotmail.com>
2023-04-25 01:03:05 +0200

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+
+const adverts = [
+  {
+      name: "Coche",
+      sale: true,
+      tags: ["lifestyle","motor"],
+      price: 15.000,
+      photo:"",
+      advertId:1
+
+  },
+  {
+      name: "Moto",
+      sale: true,
+      tags: ["lifestyle","motor"],
+      price: 10.000,
+      photo:"",
+      advertId:2
+
+  },
+  {
+      name: "Casa",
+      sale: false,
+      tags: ["lifestyle"],
+      price: 300.000,
+      photo:"",
+      advertId:3
+
+  },
+  {
+      name: "Consola",
+      sale: true,
+      tags: ["hobby","lifestyle"],
+      price: 650,
+      photo:"",
+      advertId:4
+
+  },
+  {
+      name: "Bici",
+      sale: false,
+      tags: ["lifestyle","hobby"],
+      price: 300,
+      photo:"",
+      advertId:5
+
+  },
+  
+]
+
+//export default AdvertsPage;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+          {adverts.map (advert => (
+            <li key = {advert.advertId}>{advert.name}</li>
+          ))}
+      </ul>
     </div>
   );
 }
