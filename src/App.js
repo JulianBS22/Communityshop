f07be458de062b26acc1081f505bdd1d00436809
JulianBS <julianbsuarez@gmail.com>
2023-04-28 19:22:39 +0200

    añadiendo rutas

--- src/App.js
+++ src/App.js
@@ -4,15 +4,29 @@ import LoginPage from './components/auth/LoginPage';
 import Button from './components/common/Button';
 import SignupPage from './components/auth/SignupPage';
 import { useState } from 'react';
+import { Route,Routes, Navigate } from 'react-router-dom';
 //export default AdvertsPage;
+
+const handleLogin =()=> {
+  setIsLogged(true);
+};
+
+const handleLogout =()=> {
+  setIsLogged(false);
+};
 
 function App() {
   const [isLogged, setIsLogged] = useState(false);
   return (
     <div className="App">
-    {isLogged ?<AdvertsPage /> : <LoginPage onLogin={setIsLogged}/>}      
-      <Button  variant="primary" onClick={() => window.location.assign('/api/auth/signup')}>Sign Up!!!</Button>
-      <Button  variant="secondary" onClick={event =>console.log(event)}>Login Here</Button>
+      <Routes>
+          <Route path='/login' element={<LoginPage onLogin={handleLogin}/>}  />
+          <Route path='/adverts' element={<AdvertsPage isLogged = {isLogged}/>} />
+          <Route path='/adverts/new' element= {<NewAdvertPage onLogout = {handleLogout} isLogged={isLogged}/>} />
+          <Route path='/' element= {<Navigate to ='/adverts'/>}/>
+          <Route path='*' element= {<div>404 | Not Found</div>} />
+      </Routes>
+   
     </div>
   );
 }
